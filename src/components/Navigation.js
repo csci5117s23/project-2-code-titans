8fd84cf661ce37567b3bf1719c453a0b9c6def1d
Kailashkalyan <60308577+Kailashkalyan@users.noreply.github.com>
2023-04-30 04:44:04 -0500

    Added manifest

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,43 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from 'react';
 import { UserButton } from "@clerk/nextjs"
 import {
   Container,
   Nav,
-  Navbar
+  Navbar,
+  Button
 } from "react-bootstrap";
 export default function Navigation(){
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      console.log("HI")
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setInstallable(true);
+    });
+  }, []);
+
+  const handleInstallClick = (e) => {
+      // Hide the app provided install promotion
+      setInstallable(false);
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
+  };
+
+
   return (
     <>
     <Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark">
@@ -22,6 +54,13 @@ export default function Navigation(){
               </Nav>
               <Nav>
                 <UserButton />
+              </Nav>
+              <Nav>
+              {installable &&
+          <Button variant='primary' className="install-button" onClick={handleInstallClick}>
+          INSTALL ME
+        </Button>
+        }
               </Nav>
             </Navbar.Collapse>
           </Container>

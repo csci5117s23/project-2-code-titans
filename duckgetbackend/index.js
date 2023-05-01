06eb2fe57cf0b2ceb6ac50c75caccdc300cf3221
jeevanprakash0814 <jeevanpraks@gmail.com>
2023-05-01 02:36:43 -0500

    Created yup objects for schema purposes

--- duckgetbackend/index.js
+++ duckgetbackend/index.js
@@ -3,13 +3,56 @@
 * Auto generated Codehooks (c) example
 * Install: npm i codehooks-js codehooks-crudlify
 */
-import {app} from 'codehooks-js'
+import {app,Datastore} from 'codehooks-js'
 import {crudlify} from 'codehooks-crudlify'
+import { object, string, boolean, date, number } from 'yup';
+import jwtDecode from 'jwt-decode';
+
+const PlanYup = object({
+  name: string().required(),
+  userId: string().required(),
+  location: string().required(),
+  isActive: boolean().required()
+})
+
+const PlannedExpenseYup = object({
+  name: string().required(),
+  userId: string().required(),
+  amount: number().required().positive(), // need to do a check at the frontend for whether the expense is at least $0.01
+  planId: string().required(),
+  dueDate: date().required().min(new Date()) // the date of the planned expense cannot be before today
+})
+
+const PastExpenseYup = object({
+  name: string().required(),
+  userId: string().required(),
+  amount: number().required().positive(),
+  date: date().required().max(new Date())
+})
 
 // test route for https://<PROJECTID>.api.codehooks.io/dev/
 app.get('/', (req, res) => {
   res.send('CRUD server ready')
 })
+
+// This can largely be copy-pasted, it just grabs the authorization token and parses it, stashing it on the request. from Professor Kluver's example repo
+const userAuth = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const token = authorization.replace('Bearer ','');
+      // NOTE this doesn't validate, but we don't need it to. codehooks is doing that for us.
+      const token_parsed = jwtDecode(token);
+      req.user_token = token_parsed;
+    } // might need an else here if auth not given
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(403);
+    res.json(error).end();
+  } 
+}
+app.use(userAuth)
 
 // Use Crudlify to create a REST API for any collection
 crudlify(app)
---
+++ src/modules/Data.js
@@ -0,0 +1,2 @@
+const backend_base = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+

0561180afcd185636b82fbf9ffebfc7f55c969ca
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2023-05-01 01:43:27 -0500

    i dont know what ia m commiting

--- duckgetbackend/index.js
+++ duckgetbackend/index.js
@@ -6,6 +6,15 @@
 import {app} from 'codehooks-js'
 import {crudlify} from 'codehooks-crudlify'
 
+const userYup = object({
+  uid: string().required(),
+  plans: array().of(string().required()),
+  // budgets:
+});
+const planYup = object({
+
+})
+
 // test route for https://<PROJECTID>.api.codehooks.io/dev/
 app.get('/', (req, res) => {
   res.send('CRUD server ready')
